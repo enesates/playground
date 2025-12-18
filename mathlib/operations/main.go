24af34ffef0cb5106896bddf57cd3db46257bd64
Enes Ates <enes@enesates.com>
2025-12-18 14:43:26 +0100

    mathlib operations added

---
+++ mathlib/operations/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "errors"
+)
+
+func Addition(a, b float64) float64 {
+    return a + b
+}
+
+func Subtraction(a, b float64) float64 {
+    return a - b
+}
+
+func Multiplication(a, b float64) float64 {
+    return a * b
+}
+
+func Division(a, b float64) (float64, error) {
+    if b == 0 {
+        return 0.0, errors.New("division by zero")
+    } else {
+        return a / b, nil
+    }
+}
+
+func init() {
+    println("mathlib operations initialized")
+}
