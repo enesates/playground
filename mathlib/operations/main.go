6c8575bc5d06176ccf0534f2b038abebf4e08510
Enes Ates <enes@enesates.com>
2025-12-18 15:01:06 +0100

    package name fixed

--- mathlib/operations/main.go
+++ mathlib/operations/main.go
@@ -1,29 +1,29 @@
-package main
+package operations
 
 import (
-	"errors"
+    "errors"
 )
 
 func Addition(a, b float64) float64 {
-	return a + b
+    return a + b
 }
 
 func Subtraction(a, b float64) float64 {
-	return a - b
+    return a - b
 }
 
 func Multiplication(a, b float64) float64 {
-	return a * b
+    return a * b
 }
 
 func Division(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0.0, errors.New("division by zero")
-	}
+    if b == 0 {
+        return 0.0, errors.New("division by zero")
+    }
 
-	return a / b, nil
+    return a / b, nil
 }
 
 func init() {
-	println("mathlib operations initialized")
+    println("mathlib operations initialized")
 }
