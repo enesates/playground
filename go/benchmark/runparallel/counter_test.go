46d3f5bb64054b80037b564716d77c39e94ea543
Enes Ates <enes@enesates.com>
2026-01-05 16:55:36 +0100

    database postgres and mongo

--- go/benchmark/runparallel/counter_test.go
+++ go/benchmark/runparallel/counter_test.go
@@ -1,36 +1,36 @@
 package runparallel
 
 import (
-    "testing"
+	"testing"
 )
 
 func BenchmarkCounter_AddSequential(b *testing.B) {
-    c := Counter{}
-    b.ResetTimer()
+	c := Counter{}
+	b.ResetTimer()
 
-    for i := 0; i < b.N; i++ {
-        c.Add(1)
-    }
+	for i := 0; i < b.N; i++ {
+		c.Add(1)
+	}
 }
 
 func BenchmarkCounter_AddParallelMutex(b *testing.B) {
-    mc := MutexCounter{}
-    b.ResetTimer()
+	mc := MutexCounter{}
+	b.ResetTimer()
 
-    b.RunParallel(func(pb *testing.PB) {
-        for pb.Next() {
-            mc.Add(1)
-        }
-    })
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			mc.Add(1)
+		}
+	})
 }
 
 func BenchmarkCounter_AddParallelAtomic(b *testing.B) {
-    ac := AtomicCounter{}
-    b.ResetTimer()
+	ac := AtomicCounter{}
+	b.ResetTimer()
 
-    b.RunParallel(func(pb *testing.PB) {
-        for pb.Next() {
-            ac.Add(1)
-        }
-    })
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			ac.Add(1)
+		}
+	})
 }
