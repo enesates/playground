38e7b42baf09fea735f203283e96c3e9e132f1c4
Enes Ates <enes@enesates.com>
2026-01-07 12:14:10 +0100

    testing postgres

--- go/database/postgres/helper.go
+++ go/database/postgres/helper.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "database/sql"
-    "log"
+	"database/sql"
+	"log"
 )
 
 func CloseConnection(db *sql.DB) {
-    defer func() {
-        err := db.Close()
-        if err != nil {
-            log.Fatalf("Error on closing DB connection: %v", err)
-        }
-    }()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatalf("Error on closing DB connection: %v", err)
+		}
+	}()
 }
---
+++ go/database/postgres/postgres_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestUserTableCreated(t *testing.T) {
+    t.Run("Check users table existence", func(t *testing.T) {
+        var exists bool
+
+        err := PostgresDB.QueryRow(`
+            SELECT EXISTS (
+               SELECT FROM information_schema.tables
+				WHERE table_schema = 'public'
+				AND table_name = 'users'
+            )
+        `).Scan(&exists)
+
+        if err != nil {
+            t.Fatalf("failed to check users table existence: %v", err)
+        }
+
+        if !exists {
+            t.Fatal("users table does not exist")
+        }
+    })
+}
