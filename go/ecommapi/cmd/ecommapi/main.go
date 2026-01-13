183286a00ee2115c5f90ffe03a7be59e9a1f9d82
Enes Ates <enes@enesates.com>
2026-01-13 09:54:37 +0100

    check if user exists

--- go/ecommapi/cmd/ecommapi/main.go
+++ go/ecommapi/cmd/ecommapi/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "ecommapi/internal/database"
+import (
+	// "ecommapi/docs"
+	"ecommapi/internal/database"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag/example/basic/docs"
+)
 
 func init() {
 	database.DBInit()
 }
 
 func main() {
-	database.CreateUser("test4", "test4@test.com", "assadasdsd")
+	router := gin.Default()
+	docs.SwaggerInfo.BasePath = "/"
+
+	// router.GET("/health", handlers.Health)
+	// router.GET("/auth/register", handlers.GetTodos)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	router.Run()
 }
--- go/ecommapi/internal/database/dbhelper.go
+++ go/ecommapi/internal/database/dbhelper.go
@@ -1,17 +1,32 @@
 package database
 
 import (
+	"errors"
 	"log"
 
 	"ecommapi/internal/helpers.go"
 )
 
-func CreateUser(username string, email string, password string) {
-	hashedPassword, err := helpers.HashPassword(password)
+func CheckUserExists(username string, email string) bool {
+	user := User{
+		Username: username,
+		Email:    email,
+	}
 
+	result := GormDB.First(&user)
+	return result.RowsAffected > 1
+}
+
+func CreateUser(username string, email string, password string) error {
+	if CheckUserExists(username, email) {
+		log.Printf("User already exists")
+		return errors.New("User already exists")
+	}
+
+	hashedPassword, err := helpers.HashPassword(password)
 	if err != nil {
-		log.Println("User couldn't created. Error:", err)
-		return
+		log.Printf("Error creating user: %v", err)
+		return errors.New("Internal Server Error")
 	}
 
 	user := User{
@@ -25,8 +40,30 @@ func CreateUser(username string, email string, password string) {
 
 	if result.Error != nil {
 		log.Printf("Error creating user: %v", result.Error)
-		return
+
+		return errors.New("Internal Server Error")
 	}
 
 	log.Printf("User created successfully with ID: %s", user.ID)
+	return nil
+}
+
+func GetUser(email string, password string) (User, error) {
+	var err error
+
+	user := User{
+		Email: email,
+	}
+
+	result := GormDB.First(&user)
+
+	if result.Error != nil {
+		log.Printf("Error getting user: %v", result.Error)
+		err = errors.New("Internal Server Error")
+	} else if !helpers.ComparePassword(user.PasswordHash, password) {
+		log.Printf("Credentials are not correct")
+		err = errors.New("Invalid credentials")
+	}
+
+	return user, err
 }
